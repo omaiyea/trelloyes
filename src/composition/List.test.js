d882c751626aadd1b7defa6e73a309e43ec11e37
omaiyea <omaiyea@gmail.com>
2019-08-03 21:52:53 -0700

    add snapshot test for list

--- src/composition/List.test.js
+++ src/composition/List.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import List from './List';
 import STORE from '../store.js';
 import renderer from 'react-test-renderer';
+import { exportAllDeclaration } from '@babel/types';
 
 
 /*smoke test */
@@ -10,4 +11,12 @@ it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<List cards={STORE.lists} />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+/* snapshot test */
+it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List cards={STORE.lists} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
 });
